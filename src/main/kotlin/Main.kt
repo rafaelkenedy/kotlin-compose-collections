b8799725de4716949ba8d092922a07eaa73e8ae2
rafael <k.rafaelalves@gmail.com>
2023-01-26 15:18:48 -0300

    lists, muttablelist and funs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,51 @@
 fun main() {
 
-    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
-    val gasPlanets = arrayOf<String>("Jupiter", "Saturn", "Uranus", "Neptune")
-    val solarSystem = rockPlanets + gasPlanets
+//    val rockPlanets = listOf<String>("Mercury", "Venus", "Earth", "Mars")
+//    val gasPlanets = listOf<String>("Jupiter", "Saturn", "Uranus", "Neptune")
+
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    val newSolarSystem = mutableListOf("")
+//    newSolarSystem.addAll(rockPlanets + gasPlanets)
+
+
+
+
+    //println(solarSystem[2])
+    //println(solarSystem.get(3))
+
+    //println(solarSystem.indexOf("Earth"))
+    //println(solarSystem.indexOf("Pluto"))
+
+//    for (planet in newSolarSystem){
+//        println(planet)
+//    }
+//
+
+
+    for (planet in solarSystem){
+        println(planet)
+    }
+    println(solarSystem.size)
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
 
 
     println(solarSystem[3])
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
-    val newSolarSystem = rockPlanets + gasPlanets + "Pluto"
-    println(newSolarSystem[8])
+    println(solarSystem[9])
+
+
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+
+    for (planet in solarSystem){
+        println(planet)
+    }
+
+    println(solarSystem.contains("Pluto"))
+
+    println("Future Moon" in solarSystem)
+
 
 
 }
